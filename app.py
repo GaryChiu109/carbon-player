96d598b18890f47f86bbea4e5cc4c01feb93a7da
GaryChiu109 <126947978+GaryChiu109@users.noreply.github.com>
2024-03-21 21:05:12 +0800

    app.py

--- app.py
+++ app.py
@@ -96,27 +96,55 @@ if __name__ == "__main__":
 
 import requests
 import json
-# 設定 headers，輸入你的 Access Token，記得前方要加上「Bearer 」( 有一個空白 )
-headers = {'Authorization':'Bearer 你的 uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU=','Content-Type':'application/json'}
 
-body = {
-    'size': {'width': 1080, 'height': 360},   # 設定尺寸
-    'selected': 'true',                        # 預設是否顯示
-    'name': 'Menu',                   # 選單名稱
-    'chatBarText': 'Menu',            # 選單在 LINE 顯示的標題
-    'areas':[                                  # 選單內容
-        {
-          'bounds': {'x': 0, 'y': 720, 'width': 360, 'height': 360}, # 選單位置與大小
-          'action': {'type': 'message', 'text': '為您提供氣候變遷資訊 \n '}                # 點擊後傳送文字
-        },
-        {
-          'bounds': {'x': 360, 'y': 720, 'width': 360, 'height': 360},
-          'action': {'type': 'uri', 'text': 'https://www.climatecentral.org/'}
-        }
-    ]
-  }
-# 向指定網址發送 request
-req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu',
-                      headers=headers,data=json.dumps(body).encode('utf-8'))
-# 印出得到的結果
-print(req.text)
+#LINE_CHANNEL_ACCESS_TOKEN = 'uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU='
+
+#token = LINE_CHANNEL_ACCESS_TOKEN
+
+#Authorization_token = "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
+
+# headers = {"Authorization":Authorization_token, "Content-Type":"application/json"}
+
+# body = {
+#     "size": {"width": 2500, "height": 843},
+#     "selected": "true",
+#     "name": "Menu",
+#     "chatBarText": "更多資訊",
+#     'areas': [
+#         {
+#             'bounds': {'x': 0, 'y': 0, 'width': 833, 'height': 843},
+#             'action': {"type": "message", "text": "以下網站提供了各種氣候變遷相關資訊，可供您參考: \n 1.https://www.ipcc.ch/ \n 2.https://www.climatecentral.org/ \n 3.https://unfccc.int/"}  # Make sure this text is properly encoded
+#         },
+#         {
+#             'bounds': {'x': 833, 'y': 0, 'width': 833, 'height': 843},
+#             'action': {'type': 'uri', 'uri': 'https://www.climatecentral.org/'}
+#         },
+#         {
+#             'bounds': {'x': 1666, 'y': 0, 'width': 833, 'height': 843},
+#             'action': {"type": "message", "text": "成本效益"}  # Make sure this text is properly encoded
+#         }
+#     ]
+#   }
+
+# req = requests.request('POST', 'https://api.line.me/v2/bot/richmenu',
+#                        headers=headers,data=json.dumps(body).encode('utf-8'))
+
+# print(req.text)
+
+# from linebot import (
+#     LineBotApi, WebhookHandler
+# )
+
+# line_bot_api = LineBotApi(token)
+# rich_menu_id = "richmenu-e52c01ced9f0573a185bb55f1df21c2e" # 設定成我們的 Rich Menu ID
+
+# path = "/content/drive/My Drive/linebot_menu.png" # 主選單的照片路徑
+
+# with open(path, 'rb') as f:
+#     line_bot_api.set_rich_menu_image(rich_menu_id, "image/png", f)
+
+# req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/'+rich_menu_id,
+#                        headers=headers)
+# print(req.text)
+
+# rich_menu_list = line_bot_api.get_rich_menu_list()
