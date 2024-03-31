9440b89a0f7714d876a9964c4bf8698847e8e579
GaryChiu109 <126947978+GaryChiu109@users.noreply.github.com>
2024-04-01 01:15:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,18 +75,6 @@ def reply_weather_image(reply_token):
     line_bot_api.reply_message(reply_token, message)
 
 
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    msg = event.message.text
-    if msg == '雷達回波圖' or msg == '雷達回波':
-        reply_weather_image(event.reply_token)
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
-
-
-
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
