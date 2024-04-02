a0a2ae23f577df5dea6ea642d64bdcee7ff3d121
TP6chunwei <153341491+TP6chunwei@users.noreply.github.com>
2024-04-02 14:16:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,10 @@ def linebot():
                 text = json_data['events'][0]['message']['text']
                 if text == '雷達回波圖' or text == '雷達回波':
                     reply_image(f'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0058-003.png?{time.time_ns()}', reply_token, access_token)
+                elif text == '地震資訊' or text == '地震':              # 如果是地震相關的文字
+                    msg = earth_quake()                               # 爬取地震資訊
+                    push_message(msg[0], user_id, access_token)       # 傳送地震資訊 ( 用 push 方法，因為 reply 只能用一次 )
+                    reply_image(msg[1], reply_token, access_token)    # 傳送地震圖片 ( 用 reply 方法 )
                 else:
                     reply_message(text, reply_token, access_token)    # 如果是一般文字，直接回覆同樣的文字
     except:
@@ -64,6 +68,26 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
+# 地震資訊函式
+def earth_quake():
+    msg = ['找不到地震資訊','https://example.com/demo.jpg']            # 預設回傳的訊息
+    try:
+        code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
+        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
+        e_data = requests.get(url)                                   # 爬取地震資訊網址
+        e_data_json = e_data.json()                                  # json 格式化訊息內容
+        eq = e_data_json['records']['earthquake']                    # 取出地震資訊
+        for i in eq:
+            loc = i['earthquakeInfo']['epiCenter']['location']       # 地震地點
+            val = i['earthquakeInfo']['magnitude']['magnitudeValue'] # 地震規模
+            dep = i['earthquakeInfo']['depth']['value']              # 地震深度
+            eq_time = i['earthquakeInfo']['originTime']              # 地震時間
+            img = i['reportImageURI']                                # 地震圖
+            msg = [f'{loc}，芮氏規模 {val} 級，深度 {dep} 公里，發生時間 {eq_time}。', img]
+            break     # 取出第一筆資料後就 break
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
 
 # LINE push 訊息函式
 def push_message(msg, uid, token):
@@ -177,7 +201,7 @@ def forecast(address):
             "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
     msg = '找不到天氣預報資訊。'    # 預設回傳訊息
     try:
-        code = '你的氣象開放平台授權碼'
+        code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
         url = f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization={code}&downloadType=WEB&format=JSON'
         f_data = requests.get(url)   # 取得主要縣市預報資料
         f_data_json = f_data.json()  # json 格式化訊息內容
