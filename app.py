b03295e0bd9e7744f44249cd22c1ac2ba6a247ce
TP6chunwei <153341491+TP6chunwei@users.noreply.github.com>
2024-04-02 19:24:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, abort
+import requests
+import statistics
+import os
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -6,132 +9,119 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
-
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-#======python的函數庫==========
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
+    PostbackEvent, MemberJoinedEvent
+)
 
 app = Flask(__name__)
-# Channel Access Token
-line_bot_api = LineBotApi('uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('fed72a71f8981ef1dec1e5867df85909')
 
+# Channel Access Token and Secret (Note: These should ideally be stored securely, not hardcoded)
+line_bot_api = LineBotApi('your_channel_access_token')
+handler = WebhookHandler('your_channel_secret')
 
-# 監聽所有來自 /callback 的 Post Request
+# Utility functions
+def get_data(url):
+    """Fetches data from the given URL and returns JSON."""
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    return response.json()
+
+def check_data(value):
+    """Checks if the given value is valid (not negative) and returns it."""
+    return float(value) if float(value) >= 0 else None
+
+# Weather functions
+def current_weather(address):
+    """Returns the current weather information for the given address."""
+    city_list, area_list, area_list2 = {}, {}, {}
+    msg = 'No weather information available.'
+
+    try:
+        code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
+        data_urls = [
+            f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization={code}&downloadType=WEB&format=JSON',
+            f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0003-001?Authorization={code}&downloadType=WEB&format=JSON'
+        ]
+
+        for url in data_urls:
+            w_data_json = get_data(url)
+            location = w_data_json['cwbopendata']['location']
+            for item in location:
+                name = item['locationName']
+                city = item['parameter'][0]['parameterValue']
+                area = item['parameter'][2]['parameterValue']
+                temp = check_data(item['weatherElement'][3]['elementValue']['value'])
+                humd = check_data(round(float(item['weatherElement'][4]['elementValue']['value']) * 100, 1))
+                r24 = check_data(item['weatherElement'][6]['elementValue']['value'])
+
+                area_list.setdefault(area, {'temp': temp, 'humd': humd, 'r24': r24})
+                city_list.setdefault(city, {'temp': [], 'humd': [], 'r24': []})
+                city_list[city]['temp'].append(temp)
+                city_list[city]['humd'].append(humd)
+                city_list[city]['r24'].append(r24)
+
+        for city, data in city_list.items():
+            area_list2[city] = {
+                'temp': round(statistics.mean(data['temp']), 1),
+                'humd': round(statistics.mean(data['humd']), 1),
+                'r24': round(statistics.mean(data['r24']), 1)
+            }
+
+        msg = format_weather_message(area_list2, msg)
+        msg = format_weather_message(area_list, msg)
+    except Exception as e:
+        print(f"Error fetching weather data: {e}")
+
+    return msg
+
+def format_weather_message(locations, default_msg):
+    """Formats the weather message based on the locations data."""
+    formatted_msg = default_msg
+    for location, data in locations.items():
+        if location in address:
+            temp_str = f"Temperature: {data['temp']}°C, " if data['temp'] is not None else ''
+            humd_str = f"Humidity: {data['humd']}%, " if data['humd'] is not None else ''
+            r24_str = f"Rainfall: {data['r24']}mm" if data['r24'] is not None else ''
+            description = f'{temp_str}{humd_str}{r24_str}'.strip(', ')
+            formatted_msg = f'{description}.'
+            break
+    return formatted_msg
+
+def reply_weather_image(reply_token):
+    """Replies with the latest weather radar image."""
+    try:
+        radar_url = 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0058-003?Authorization=rdec-key-123-45678-011121314&format=JSON'
+        radar_json = get_data(radar_url)
+        radar_img = radar_json['cwaopendata']['dataset']['resource']['ProductURL']
+
+        line_bot_api.reply_message(
+            reply_token,
+            ImageSendMessage(
+                original_content_url=radar_img,
+                preview_image_url=radar_img
+            )
+        )
+    except Exception as e:
+        print(f"Error replying with weather image: {e}")
+
+# Route for handling webhook callback
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'
 
-access_token = 'uyT/wIyH6kkz35o7X7G8Edzgisq8l4Vn1wTvz+QMXcuKAnaXUhYucEHjaZKRXgAVnYvk3DfMhcsF60/iA6NxzaKgo0SPOb/yn7xLZxmTfzegtqB2J1na74r8SAo2aZCuBsw/+pdnfCLolxSvvD+6lwdB04t89/1O/w1cDnyilFU='
-
-
-# Radar image
-def reply_weather_image(reply_token):
-    radar_url = 'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0058-003?Authorization=rdec-key-123-45678-011121314&format=JSON'
-    radar = requests.get(radar_url)
-    radar_json = radar.json()
-    radar_img = radar_json['cwaopendata']['dataset']['resource']['ProductURL']
-    radar_time = radar_json['cwaopendata']['dataset']['DateTime'] 
-
-    line_bot_api.reply_message(
-        reply_token,
-        ImageSendMessage(
-            original_content_url = radar_img,
-            preview_image_url = radar_img
-        )
-    )
-    #req = requests.request('POST', 'https://api.line.me/v2/bot/message/reply', headers=headers,data=json.dumps(body).encode('utf-8'))
-    print(radar_img)
-    #line_bot_api.reply_message(reply_token, message)
-
-
-# 目前天氣函式
-def current_weather(address):
-    city_list, area_list, area_list2 = {}, {}, {} # 定義好待會要用的變數
-    msg = '找不到氣象資訊。'                         # 預設回傳訊息
-
-    # 定義取得資料的函式
-    def get_data(url):
-        w_data = requests.get(url)   # 爬取目前天氣網址的資料
-        w_data_json = w_data.json()  # json 格式化訊息內容
-        location = w_data_json['cwbopendata']['location']  # 取出對應地點的內容
-        for i in location:
-            name = i['locationName']                       # 測站地點
-            city = i['parameter'][0]['parameterValue']     # 縣市名稱
-            area = i['parameter'][2]['parameterValue']     # 鄉鎮行政區
-            temp = check_data(i['weatherElement'][3]['elementValue']['value'])                       # 氣溫
-            humd = check_data(round(float(i['weatherElement'][4]['elementValue']['value'] )*100 ,1)) # 相對濕度
-            r24 = check_data(i['weatherElement'][6]['elementValue']['value'])                        # 累積雨量
-            if area not in area_list:
-                area_list[area] = {'temp':temp, 'humd':humd, 'r24':r24}  # 以鄉鎮區域為 key，儲存需要的資訊
-            if city not in city_list:
-                city_list[city] = {'temp':[], 'humd':[], 'r24':[]}       # 以主要縣市名稱為 key，準備紀錄裡面所有鄉鎮的數值
-            city_list[city]['temp'].append(temp)   # 記錄主要縣市裡鄉鎮區域的溫度 ( 串列格式 )
-            city_list[city]['humd'].append(humd)   # 記錄主要縣市裡鄉鎮區域的濕度 ( 串列格式 )
-            city_list[city]['r24'].append(r24)     # 記錄主要縣市裡鄉鎮區域的雨量 ( 串列格式 )
-
-    # 定義如果數值小於 0，回傳 False 的函式
-    def check_data(e):
-        return False if float(e)<0 else float(e)
-
-    # 定義產生回傳訊息的函式
-    def msg_content(loc, msg):
-        a = msg
-        for i in loc:
-            if i in address: # 如果地址裡存在 key 的名稱
-                temp = f"氣溫 {loc[i]['temp']} 度，" if loc[i]['temp'] != False else ''
-                humd = f"相對濕度 {loc[i]['humd']}%，" if loc[i]['humd'] != False else ''
-                r24 = f"累積雨量 {loc[i]['r24']}mm" if loc[i]['r24'] != False else ''
-                description = f'{temp}{humd}{r24}'.strip('，')
-                a = f'{description}。' # 取出 key 的內容作為回傳訊息使用
-                break
-        return a
-
-    try:
-        # 因為目前天氣有兩組網址，兩組都爬取
-        code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
-        get_data(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization={code}&downloadType=WEB&format=JSON')
-        get_data(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0003-001?Authorization={code}&downloadType=WEB&format=JSON')
-
-        for i in city_list:
-            if i not in area_list2: # 將主要縣市裡的數值平均後，以主要縣市名稱為 key，再度儲存一次，如果找不到鄉鎮區域，就使用平均數值
-                area_list2[i] = {'temp':round(statistics.mean(city_list[i]['temp']),1),
-                                'humd':round(statistics.mean(city_list[i]['humd']),1),
-                                'r24':round(statistics.mean(city_list[i]['r24']),1)
-                                }
-        msg = msg_content(area_list2, msg)  # 將訊息改為「大縣市」
-        msg = msg_content(area_list, msg)   # 將訊息改為「鄉鎮區域」
-        return msg    # 回傳 msg
-    except:
-        return msg    # 如果取資料有發生錯誤，直接回傳 msg
-
-
-# 處理訊息
+# Message handling
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg= event.message.text
-    if msg == '雷達回波圖' or msg == '雷達回波' or msg == 'radar':
+    msg = event.message.text
+    if msg.lower() in ['雷達回波圖', '雷達回波', 'radar']:
         reply_weather_image(event.reply_token)
     elif '台' in msg or '臺' in msg:
         weather_forecast = current_weather(msg)
@@ -140,23 +130,20 @@ def handle_message(event):
         message = TextSendMessage(text=msg)
         line_bot_api.reply_message(event.reply_token, message)
 
-
+# Other events handling
 @handler.add(PostbackEvent)
-def handle_message(event):
+def handle_postback(event):
     print(event.postback.data)
 
-
 @handler.add(MemberJoinedEvent)
-def welcome(event):
+def handle_member_join(event):
     uid = event.joined.members[0].user_id
     gid = event.source.group_id
     profile = line_bot_api.get_group_member_profile(gid, uid)
     name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
+    message = TextSendMessage(text=f'Welcome, {name}!')
     line_bot_api.reply_message(event.reply_token, message)
 
-        
-import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
