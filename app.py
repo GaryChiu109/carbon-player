f7ae27b07c9c0627efdc1987e8b732db6aff72a0
TP6chunwei <153341491+TP6chunwei@users.noreply.github.com>
2024-04-03 17:09:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,60 +35,149 @@ def check_data(value):
 
 # Weather functions
 def current_weather(address):
-    """Returns the current weather information for the given address."""
-    city_list, area_list, area_list2 = {}, {}, {}
-    msg = 'No weather information available.'
+    city_list, town_list, town_list2 = {}, {}, {}
+    msg = '找不到氣象資訊。'
 
+    def get_data(url):
+        data = requests.get(url)
+        data_json = data.json()
+        station = data_json['records']['Station']
+        for i in station:
+            city = i['GeoInfo']['CountyName'] # 城市
+            town = i['GeoInfo']['TownName'] # 行政區
+            prec = check_data(i['WeatherElement']['Now']['Precipitation']) # 即時降雨量
+            airtemp = check_data(i['WeatherElement']['AirTemperature']) # 氣溫
+            humd = check_data(i['WeatherElement']['AirTemperature']) # 相對溼度
+            dailyhigh = check_data(i['WeatherElement']['DailyExtreme']['DailyHigh']['TemperatureInfo']['AirTemperature']) # 當日最高溫
+            dailylow = check_data(i['WeatherElement']['DailyExtreme']['DailyLow']['TemperatureInfo']['AirTemperature']) # 當日最低溫
+            if town not in town_list:
+                town_list[town] = {'prec': prec, 'airtemp': airtemp, 'humd': humd, 
+                                    'dailyhigh': dailyhigh, 'dailylow': dailylow}
+            if city not in city_list:
+                city_list[city] = {'prec': [], 'airtemp': [], 'humd': [],
+                                'dailyhigh': [], 'dailylow': []}
+            city_list[city]['prec'].append(prec)
+            city_list[city]['airtemp'].append(airtemp)
+            city_list[city]['humd'].append(humd)
+            city_list[city]['dailyhigh'].append(dailyhigh)
+            city_list[city]['dailylow'].append(dailylow)
+        return city_list
+
+    # 如果數值小於0，回傳False
+    def check_data(e):
+        return False if float(e) < 0 else float(e)
+
+    # 產生回傳訊息
+    def msg_content(loc, msg):
+        for i in loc:
+            if i in address: # 如果地址裡存在 key 的名稱
+                airtemp = f"氣溫 {loc[i]['airtemp']} 度，" if loc[i]['airtemp'] != False else ''
+                humd = f"相對濕度 {loc[i]['humd']}%，" if loc[i]['humd'] != False else ''
+                prec = f"累積雨量 {loc[i]['prec']}mm" if loc[i]['prec'] != False else ''
+                description = f'{airtemp}{humd}{prec}'.strip('，')
+                a = f'{description}。' # 取出 key 的內容作為回傳訊息使用
+                break
+        return a
+    
     try:
         code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
-        data_urls = [
-            f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization={code}&downloadType=WEB&format=JSON',
-            f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0003-001?Authorization={code}&downloadType=WEB&format=JSON'
-        ]
+        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}&format=JSON'
+        get_data(url)
 
-        for url in data_urls:
-            w_data_json = get_data(url)
-            location = w_data_json['cwbopendata']['location']
-            for item in location:
-                name = item['locationName']
-                city = item['parameter'][0]['parameterValue']
-                area = item['parameter'][2]['parameterValue']
-                temp = check_data(item['weatherElement'][3]['elementValue']['value'])
-                humd = check_data(round(float(item['weatherElement'][4]['elementValue']['value']) * 100, 1))
-                r24 = check_data(item['weatherElement'][6]['elementValue']['value'])
+        for i in city_list:
+            if i not in town_list2: # 將主要縣市裡的數值平均後，以主要縣市名稱為 key，再度儲存一次，如果找不到鄉鎮區域，就使用平均數值
+                town_list2[i] = {'airtemp':round(statistics.mean(city_list[i]['airtemp']),1), 
+                                'humd':round(statistics.mean(city_list[i]['humd']),1), 
+                                'prec':round(statistics.mean(city_list[i]['prec']),1)
+                                }
+        msg = msg_content(town_list2, msg)  # 將訊息改為「大縣市」 
+        msg = msg_content(town_list, msg)   # 將訊息改為「鄉鎮區域」 
+        return msg, town_list    # 回傳 msg
+    except:
+        return msg
+print(current_weather('臺灣臺北市大安區基隆路四段144巷67號'))
+            
+# 氣象預報函式
+def forecast(address):
+    area_list = {}
+    # 將主要縣市個別的 JSON 代碼列出
+    json_api = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
+            "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
+            "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
+            "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
+            "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
+            "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
+    msg = '找不到天氣預報資訊。'    # 預設回傳訊息
+    try:
+        code = 'CWA-371EFA85-E086-45AE-B068-E449E4478D6A'
+        url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={code}&format=JSON'
+        f_data = requests.get(url)   # 取得主要縣市預報資料
+        f_data_json = f_data.json()  # json 格式化訊息內容
+        location = f_data_json['records']['location']  # 取得縣市的預報內容
+        for i in location:
+            city = i['locationName']    # 縣市名稱
+            wx8 = i['weatherElement'][0]['time'][0]['parameter']['parameterName']    # 天氣現象
+            mint8 = i['weatherElement'][1]['time'][0]['parameter']['parameterName']  # 最低溫
+            maxt8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']  # 最高溫
+            pop8 = i['weatherElement'][2]['time'][0]['parameter']['parameterName']   # 降雨機率
+            area_list[city] = f'未來 8 小時{wx8}，最高溫 {maxt8} 度，最低溫 {mint8} 度，降雨機率 {pop8} %'  # 組合成回傳的訊息，存在以縣市名稱為 key 的字典檔裡
+        for i in area_list:
+            if i in address:        # 如果使用者的地址包含縣市名稱
+                msg = area_list[i]  # 將 msg 換成對應的預報資訊
+                # 將進一步的預報網址換成對應的預報網址
+                url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/{json_api[i]}?Authorization={code}&elementName=WeatherDescription'
+                f_data = requests.get(url)  # 取得主要縣市裡各個區域鄉鎮的氣象預報
+                f_data_json = f_data.json() # json 格式化訊息內容
+                location = f_data_json['records']['locations'][0]['location']    # 取得預報內容
+                break
+        for i in location:
+            city = i['locationName']   # 取得縣市名稱
+            wd = i['weatherElement'][0]['time'][1]['elementValue'][0]['value']  # 綜合描述
+            if city in address:           # 如果使用者的地址包含鄉鎮區域名稱
+                msg = f'未來八小時天氣{wd}' # 將 msg 換成對應的預報資訊
+                break
+        return msg  # 回傳 msg
+    except:
+        return msg  # 如果取資料有發生錯誤，直接回傳 msg
 
-                area_list.setdefault(area, {'temp': temp, 'humd': humd, 'r24': r24})
-                city_list.setdefault(city, {'temp': [], 'humd': [], 'r24': []})
-                city_list[city]['temp'].append(temp)
-                city_list[city]['humd'].append(humd)
-                city_list[city]['r24'].append(r24)
-
-        for city, data in city_list.items():
-            area_list2[city] = {
-                'temp': round(statistics.mean(data['temp']), 1),
-                'humd': round(statistics.mean(data['humd']), 1),
-                'r24': round(statistics.mean(data['r24']), 1)
-            }
-
-        msg = format_weather_message(area_list2, msg)
-        msg = format_weather_message(area_list, msg)
-    except Exception as e:
-        print(f"Error fetching weather data: {e}")
-
-    return msg
-
-def format_weather_message(locations, default_msg):
-    """Formats the weather message based on the locations data."""
-    formatted_msg = default_msg
-    for location, data in locations.items():
-        if location in address:
-            temp_str = f"Temperature: {data['temp']}°C, " if data['temp'] is not None else ''
-            humd_str = f"Humidity: {data['humd']}%, " if data['humd'] is not None else ''
-            r24_str = f"Rainfall: {data['r24']}mm" if data['r24'] is not None else ''
-            description = f'{temp_str}{humd_str}{r24_str}'.strip(', ')
-            formatted_msg = f'{description}.'
-            break
-    return formatted_msg
+# 空氣品質函式
+def aqi(address):
+    city_list, site_list ={}, {}
+    msg = '找不到空氣品質資訊。'
+    try:
+        # 2022/12 時氣象局有修改了 API 內容，將部份大小寫混合全改成小寫，因此程式碼也跟著修正
+        url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+        a_data = requests.get(url)             # 使用 get 方法透過空氣品質指標 API 取得內容
+        a_data_json = a_data.json()            # json 格式化訊息內容
+        for i in a_data_json['records']:       # 依序取出 records 內容的每個項目
+            city = i['county']                 # 取出縣市名稱
+            if city not in city_list:
+                city_list[city]=[]             # 以縣市名稱為 key，準備存入串列資料
+            site = i['sitename']               # 取出鄉鎮區域名稱
+            aqi = int(i['aqi'])                # 取得 AQI 數值
+            status = i['status']               # 取得空氣品質狀態
+            site_list[site] = {'aqi':aqi, 'status':status}  # 記錄鄉鎮區域空氣品質
+            city_list[city].append(aqi)        # 將各個縣市裡的鄉鎮區域空氣 aqi 數值，以串列方式放入縣市名稱的變數裡
+        for i in city_list:
+            if i in address: # 如果地址裡包含縣市名稱的 key，就直接使用對應的內容
+                # 參考 https://airtw.epa.gov.tw/cht/Information/Standard/AirQualityIndicator.aspx
+                aqi_val = round(statistics.mean(city_list[i]),0)  # 計算平均數值，如果找不到鄉鎮區域，就使用縣市的平均值
+                aqi_status = ''  # 手動判斷對應的空氣品質說明文字
+                if aqi_val<=50: aqi_status = '良好'
+                elif aqi_val>50 and aqi_val<=100: aqi_status = '普通'
+                elif aqi_val>100 and aqi_val<=150: aqi_status = '對敏感族群不健康'
+                elif aqi_val>150 and aqi_val<=200: aqi_status = '對所有族群不健康'
+                elif aqi_val>200 and aqi_val<=300: aqi_status = '非常不健康'
+                else: aqi_status = '危害'
+                msg = f'空氣品質{aqi_status} ( AQI {aqi_val} )。' # 定義回傳的訊息
+                break
+        for i in site_list:
+            if i in address:  # 如果地址裡包含鄉鎮區域名稱的 key，就直接使用對應的內容
+                msg = f'空氣品質{site_list[i]["status"]} ( AQI {site_list[i]["aqi"]} )。'
+                break
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
 
 def reply_weather_image(reply_token):
     """Replies with the latest weather radar image."""
@@ -124,10 +213,7 @@ def callback():
 def handle_message(event):
     if event.message.type == 'location':
         address = event.message.address.replace('台', '臺')
-        lat = event.message.latitude
-        lon = event.message.longitude
-        reply = f"The address is {address}. \nThe latitude is {lat} and the longitude is {lon}."
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+        line_bot_api.reply_message(event.reply_token, message=TextSendMessage(text=f'{address}\n\n{current_weather(address)}\n\n{aqi(address)}\n\n{forecast(address)}'))
     elif  event.message.type == 'text':
         msg = event.message.text
         if msg.lower() in ['雷達回波圖', '雷達回波', 'radar']:
